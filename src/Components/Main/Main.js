4e172fbebe0c67f97f708f64ab126272801bd59a
Soheil Sadeghbayan <soheilsadeghbayan@yahoo.com>
2020-09-04 00:22:40 +0430

    add config and util files for scss

---
+++ src/Components/Main/Main.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import styles from './Main.module.scss'
+
+
+class Main extends Component {
+	render() {
+		return (
+			<div className={styles['main-wrapper']}>
+				Test
+			</div>
+		);
+	}
+}
+
+export default Main;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import '../src/scss/style.scss';
+import Main from './Components/Main/Main'
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-      <>Hi</>
+     <Main />
   </React.StrictMode>,
   document.getElementById('root')
 );
