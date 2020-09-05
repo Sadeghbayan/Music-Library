344136f036affae1d1c3b953fef1e542eaae08f4
Soheil Sadeghbayan <soheilsadeghbayan@yahoo.com>
2020-09-06 00:53:24 +0430

    add fonts and center layout

--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -1,13 +1,17 @@
 import React, {Component} from 'react';
+import Header from "../Header/Header"
 import styles from './Main.module.scss'
 
-
 class Main extends Component {
+
 	render() {
 		return (
-			<div className={styles['main-wrapper']}>
-				Test
-			</div>
+				<div className={styles['main-wrapper']}>
+					<Header/>
+					<section>
+
+					</section>
+				</div>
 		);
 	}
 }
