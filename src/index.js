fac6099ce392269addd30c6d5267a228959bbf89
Soheil Sadeghbayan <soheilsadeghbayan@yahoo.com>
2020-09-06 00:54:03 +0430

    remove extra spaces on index js

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-     <Main />
+      <Main />
   </React.StrictMode>,
   document.getElementById('root')
 );
