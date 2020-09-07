bb7138f3656ccf61f9f5254412df5d1050a0edc4
Soheil Sadeghbayan <soheilsadeghbayan@yahoo.com>
2020-09-07 19:06:56 +0430

    add filter comp to main comp

--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -1,15 +1,29 @@
 import React, {Component} from 'react';
 import Header from "../Header/Header"
+import Filter from "../Filter/Filter"
 import styles from './Main.module.scss'
 
 class Main extends Component {
 
+	state = {
+		activeStatus : false
+	}
+
 	render() {
+
+		const handleClick = (active) => {
+			this.setState({
+				activeStatus: !this.state.activeStatus
+			})
+		}
+
+		const { activeStatus } = this.state
+
 		return (
 				<div className={styles['main-wrapper']}>
 					<Header/>
 					<section>
-
+						<Filter active={activeStatus} handleClick={handleClick} />
 					</section>
 				</div>
 		);
